59f56dd20c4bfd62f6604c64a92d77ff756391f7
javin141 <66424374+javin141@users.noreply.github.com>
2023-02-01 13:35:08 +0800

    amongus

--- Lab3/Lab3.ino
+++ Lab3/Lab3.ino
@@ -33,23 +33,23 @@ char x;
 int y, z;
 
 void setup() {
-	pinMode(9, OUTPUT);
-	pinMode(6, OUTPUT);
-	pinMode(5, OUTPUT);
-	pinMode(3, OUTPUT);
-	pinMode(4,INPUT_PULLUP);
-	pinMode(7,INPUT_PULLUP);
-	pinMode(4,INPUT_PULLUP); //IR1 right
-	pinMode(2,INPUT_PULLUP);//IR2 left
-	Serial.begin(9600);
+  pinMode(9, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP); //IR1 right
+  pinMode(2,INPUT_PULLUP);//IR2 left
+  Serial.begin(9600);
         pinMode(tripin, OUTPUT);
         pinMode(echopin, INPUT);
-	pinMode(buzzer,OUTPUT);
+  pinMode(buzzer,OUTPUT);
         lcd.init();
         lcd.setCursor(0,0);
         lcd.begin(16, 2); // 16 characters, 2 lines
         lcd.backlight();  // turn backlight on
-		Pathfindr();
+    Pathfindr();
 }
 
 // the loop function runs over and over again until power down or reset
@@ -59,89 +59,11 @@ void setup() {
 // z -> delay
 // v -> speed
 void loop() {
-	lcd.println ("Path Clear");
- 	IR();
-	ultrasound();
+  lcd.clear();
+  IR();
 }
 
-void move(int x, int y, int z, int v)
-{
-	int c, d;
-	switch (y)
-	{
-	case 1:
-	{
-		c = 0;
-		d = 0;
-		// no run
-	}
-		break;
-	case 2:
-	{
-		c = 0;
-		d = v;
-		// forward
-	}
-		break;
-	case 3:
-	{
-		c = v;
-		d = 0;
-		// reverse
-	}
-		break;
-	case 4:
-	{
-		c = 200;
-		d = 200;
-		// brake
-	}
-		break;
-	}
-		switch (x)
-		{
-		case 1:
-		{
-			analogWrite(left_a, c);
-			analogWrite(left_b, d);
-			delay(z);
-			analogWrite(left_a, 200);
-			analogWrite(left_b, 200);
-			return;
-			// left
-		}
-		break;
-		case 2:
-		{
-			analogWrite(right_a, c);
-			analogWrite(right_b, d);
-			delay(z);
-			analogWrite(right_a, 200);
-			analogWrite(right_b, 200);
-			return;
-			// right
-		}
-		break;
-		case 3:
-		{
-			analogWrite(left_a, c);
-			analogWrite(left_b, d);
-			analogWrite(right_a, c);
-			analogWrite(right_b, d);
-			delay(z);
-			analogWrite(left_a, 200);
-			analogWrite(left_b, 200);
-			analogWrite(right_a, 200);
-			analogWrite(right_b, 200);
-			return;
-			// both
-		}
-		break;
-		}
-}
-
-void IR(void)
-{
+void IR(void) {
 IR1_Val = digitalRead(IR1Pin); // Reading and storing IR sensor 1 signal value
 IR2_Val = digitalRead(IR2Pin); // Reading and storing IR sensor 2 signal value
   // 1=black 0=white pin2,IR2=left pin4,IR1=right
@@ -171,19 +93,95 @@ lcd.println("stop");
  lcd.clear();
 }
 
+void move(int x, int y, int z, int v)
+{
+  int c, d;
+  switch (y)
+  {
+  case 1:
+  {
+    c = 0;
+    d = 0;
+    // no run
+  }
+    break;
+  case 2:
+  {
+    c = 0;
+    d = v;
+    // forward
+  }
+    break;
+  case 3:
+  {
+    c = v;
+    d = 0;
+    // reverse
+  }
+    break;
+  case 4:
+  {
+    c = 200;
+    d = 200;
+    // brake
+  }
+    break;
+  }
+    switch (x)
+    {
+    case 1:
+    {
+      analogWrite(left_a, c);
+      analogWrite(left_b, d);
+      delay(z);
+      analogWrite(left_a, 200);
+      analogWrite(left_b, 200);
+      return;
+      // left
+    }
+    break;
+    case 2:
+    {
+      analogWrite(right_a, c);
+      analogWrite(right_b, d);
+      delay(z);
+      analogWrite(right_a, 200);
+      analogWrite(right_b, 200);
+      return;
+      // right
+    }
+    break;
+    case 3:
+    {
+      analogWrite(left_a, c);
+      analogWrite(left_b, d);
+      analogWrite(right_a, d);
+      analogWrite(right_b, c);
+      delay(z);
+      analogWrite(left_a, 200);
+      analogWrite(left_b, 200);
+      analogWrite(right_a, 200);
+      analogWrite(right_b, 200);
+      return;
+      // both
+    }
+    break;
+    } 
+}
+
 
 void ultrasound (void)
 {
-	digitalWrite(tripin, HIGH);
+  digitalWrite(tripin, HIGH);
         delayMicroseconds(10);
         digitalWrite(tripin, LOW);
         delayMicroseconds(10);
         int dist = pulseIn(echopin, HIGH) / 58;
-	while (dist<10)
+  while (dist<10)
   {
-   	lcd.println("Path Blocked!!!");
-	move (3,4,2000,200);
-	lcd.clear();
+    lcd.println("Path Blocked!!!");
+  move (3,4,2000,200);
+  lcd.clear();
   }
   return;
 }
@@ -200,10 +198,10 @@ void lcdscreen(int address)
 
 void buzzersoundsound (void)
 {
-	for(int i=0;i<11;i++)
+  for(int i=0;i<11;i++)
     {
-		digitalWrite(buzzer,HIGH);
-		delay(1);
+    digitalWrite(buzzer,HIGH);
+    delay(1);
                 digitalWrite(buzzer,LOW);
                 delay(1);
     }
@@ -212,17 +210,17 @@ void buzzersoundsound (void)
 
 void Pathfindr(void)
 {
-	for (int il = 0; il < 3; il++)
-	{
-		for (int yl = 0; yl < 3; yl++)
-		{
-			if (AddrList[il][yl] != mohg)
-			{}
-			else 
-			{
-				return;
-			}
-			
-		}
-	}
+  for (int il = 0; il < 3; il++)
+  {
+    for (int yl = 0; yl < 3; yl++)
+    {
+      if (AddrList[il][yl] != mohg)
+      {}
+      else 
+      {
+        return;
+      }
+      
+    }
+  }
 }
